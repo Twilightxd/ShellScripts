1e32ce9af57fc0c465b26af3f86198ab75dfc3a7
iTwili8 <143506052+Twilightxd@users.noreply.github.com>
2025-05-19 08:54:15 +0530

    Update backupScript.sh

--- backupScript.sh
+++ backupScript.sh
@@ -3,11 +3,11 @@
 #Manas
 #Date : 19 May
 
-#This script helps in backing up your directory
+#This script backs up a folder to a backup directory
 
 
-src="/home/iTwili8/Documents"			#provide the path for the directory you want to backup
-dest="/home/iTwili8/Backup"			#provide the backup folder(directory) path
+src="/home/iTwili8/Documents"			#Provide the path for the directory you want to backup
+dest="/home/iTwili8/Backup"			  #Provide the backup folder(directory) path
 
 mkdir -p "$dest"
 cp -r "$src" "$dest"
