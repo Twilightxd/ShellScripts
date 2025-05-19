5146beff527927739efe8a7f6859683a317cdb79
iTwili8 <143506052+Twilightxd@users.noreply.github.com>
2025-05-19 08:52:11 +0530

    Update nodeHealth.sh

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -3,15 +3,16 @@
 #iTwili8-Manas
 #Date : 19 May
 
-#This Script outputs the node health
+#This Script outputs the node health by showing disk, memory, CPU usage
 #Version : v1
 
-df -h
-free -g
-nproc
+df -h        #Prints disk-sapce in human readable format (GB/MB etc)
+free -g      #Prints memory usage in GB
+nproc        #Prints number of CPU cores available
 
 
 #Now the same commands where i manually input the details about the commands
+#'echo' command - Prints label enclosed within ""
 echo "Print Memory"
 df -h
 echo "Print CPU"
@@ -21,7 +22,8 @@ free -g
 
 
 #Now same commands with the help of 'set' command
-set -x  #means debug mode
+#'set' command - Runs commands with shell debug mode to trace execution
+set -x       #means debug mode, shows each command before execution
 df -h
 nproc
 free -g
