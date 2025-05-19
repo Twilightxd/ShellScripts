5f67cdee2c53e05c5a7f3375933ad8d3679f3e38
iTwili8 <143506052+Twilightxd@users.noreply.github.com>
2025-05-19 07:36:16 +0530

    Add files via upload

---
+++ looping1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Manas
+#Date : 19 May
+
+#This Script covers the if if..else statements and a simple for loop
+#Version : v1
+
+a=4		#no space to be given while assigning variables
+b=10
+if [ $a -gt $b ]		#'$' indicates variable and '-gt' indicates numeric comparision
+then
+	echo "a is greater than b"		#'echo' similar to SOPln/printf...etc
+else
+	echo "b is greater than a"
+fi
+
+
+#for loop example
+for i in loop{1..100}; do echo $i; done
+
+#1st part = condition
+#2nd part = action
+#3rd part = completion or exit the loop
+
+#Can be also done as:
+for i in loop{1..100}
+do
+	echo "Loop iteration: $i"
+done
+
+#END
---
+++ nodeHealth.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Manas
+#Date : 19 May
+
+#This Script outputs the node health
+#Version : v1
+
+df -h
+free -g
+nproc
+
+
+#Now revising the same commands where i manually input the commands 
+echo "Print Memory"
+df -h
+echo "Print CPU"
+nproc
+echo "Print Storage"
+free -g
+
+
+#Now same commands with the help of 'set' command
+set -x  #means debug mode
+df -h
+nproc
+free -g
+
+#END
